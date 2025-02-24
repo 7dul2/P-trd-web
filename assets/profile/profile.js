1094ac4ea62aec8fcdbca574b6e0d44751e65352
7dul2 <953007962@qq.com>
2025-02-24 09:59:48 +0800

    profile页面优化 增加鸣谢 移除赞助者

--- assets/profile/profile.js
+++ assets/profile/profile.js
@@ -1,15 +1,6 @@
 // 个性化
 
 // 涨跌颜色
-function trend_color_load(){
-    var status = load_trend_color_preference();
-    if (status == "bearish"){
-        document.getElementById("trend_color_text").innerHTML = "<up>红涨</up><down>绿跌</down>"
-    }else {
-        document.getElementById("trend_color_text").innerHTML = "<up>绿涨</up><down>红跌</down>"
-    }
-}
-trend_color_load();
 document.getElementById("trend_color_preference").onclick = function(){
     var status = load_trend_color_preference();
     if (status == "bearish"){
